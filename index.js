445c8a3b0ffb08eeeed1abf1c76d42f38a22a101
Adnan Niloy <apache_adnan1313@hotmail.com>
2018-05-21 20:35:57 +0600

    added put in express

--- index.js
+++ index.js
@@ -34,14 +34,11 @@ app.get('/api/posts/:year/:month', (req, res) => {
 //#region post
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+    
+    const { error } = validateCourse(req.body)
 
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -54,6 +51,33 @@ app.post('/api/courses', (req, res) => {
 });
 
 //#endregion
+
+//#region put
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given id was not found!');
+
+    const { error } = validateCourse(req.body)
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+//#endregion
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+};
+
 const port = process.env.port || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
