14464fdd9d6d7da4c6898a5002eb14f7e5d77148
Adnan-Niloy <adnanniloy1313@gmail.com>
2018-05-25 02:40:02 +0600

    middleware start

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+app.use(logger);
 
 const courses = [
     { id: 1, name: 'course1' },
